e2eb5808d79bfa8e5bb695739dcc0721208ad737
Gabriel <contato.medicistore@gmail.com>
2026-06-07 03:08:02 -0300

    style: aumenta logo em +15%

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -29,7 +29,7 @@ export default function HeroSection() {
                 <img 
                   src="/images/logo_efeito_rebote.png" 
                   alt="Logo Efeito Rebote" 
-                  className="w-40 md:w-64 lg:w-72 h-auto drop-shadow-2xl" 
+                  className="w-48 md:w-72 lg:w-96 h-auto drop-shadow-2xl" 
                 />
               </div>
             </div>
